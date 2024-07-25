6ae4f7394b85f9d9e45ba04faf6451609fa16b83
subindevus <hotline.hayday@gmail.com>
2024-07-25 13:12:56 +0700

    :)

--- script.js
+++ script.js
@@ -3,71 +3,25 @@ function getQueryParameter(name) {
     return urlParams.get(name);
 }
 
-async function fetchData(fileName) {
-    const response = await fetch(fileName);
+async function fetchData() {
+    const response = await fetch('data.json');
     const data = await response.json();
     return data;
 }
 
-async function updateKeyUsage(key) {
-    const keyData = await fetchData('key.json');
-    if (keyData[key] > 0) {
-        keyData[key]--;
-        await fetch('key.json', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(keyData)
-        });
-        return true;
-    }
-    return false;
-}
-
 function findAnswer(question, data) {
     const result = data.find(item => item.question.toLowerCase() === question.toLowerCase());
-    return result ? result.answer : null;
+    return result ? result.answer : "Em hog hiểu anh đang nói gì !";
 }
 
 async function main() {
     const question = getQueryParameter('question');
-    const key = getQueryParameter('key');
-
-    if (!key) {
-        document.body.innerText = JSON.stringify({ status: 400, message: "Thiếu key !" });
-        return;
-    }
-
-    const keyData = await fetchData('key.json');
-
-    if (!(key in keyData)) {
-        document.body.innerText = JSON.stringify({ status: 403, message: "Key sai !" });
-        return;
-    }
-
-    if (keyData[key] <= 0) {
-        document.body.innerText = JSON.stringify({ status: 429, message: "Key đã hết số lượt dùng !" });
-        return;
-    }
-
-    if (!question) {
-        document.body.innerText = "Created By Subin Dev";
-        return;
-    }
-
-    const data = await fetchData('data.json');
-    const answer = findAnswer(question, data);
-
-    const updated = await updateKeyUsage(key);
-
-    if (!updated) {
-        document.body.innerText = JSON.stringify({ status: 500, message: "Lỗi khi cập nhật key !" });
-        return;
-    }
-
-    if (answer) {
-        document.body.innerText = JSON.stringify({ status: 200, answer: answer });
+    if (question) {
+        const data = await fetchData();
+        const answer = findAnswer(question, data);
+        document.body.innerText = JSON.stringify({ answer: answer });
     } else {
-        document.body.innerText = JSON.stringify({ status: 200, message: "Em hok hiểu anh đang nói gì !" });
+        document.body.innerText = "API CREATE BY SUBIN DEV <3";
     }
 }
 
