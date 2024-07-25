671b549980ff43c7e7a1ae18762ebab249ff7ab6
subindevus <hotline.hayday@gmail.com>
2024-07-25 12:30:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-// Function to get the value of a query parameter
 function getQueryParameter(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
 
-// Function to fetch data from data.json
 async function fetchData() {
     const response = await fetch('data.json');
     const data = await response.json();
     return data;
 }
 
-// Function to find the answer to the given question
 function findAnswer(question, data) {
     const result = data.find(item => item.question.toLowerCase() === question.toLowerCase());
-    return result ? result.answer : "Question not found";
+    return result ? result.answer : "Em hok hiểu anh nói gì hết !!";
 }
 
-// Main function to process the question and display the answer
 async function main() {
     const question = getQueryParameter('question');
     if (question) {
@@ -25,9 +21,8 @@ async function main() {
         const answer = findAnswer(question, data);
         document.body.innerText = JSON.stringify({ answer: answer });
     } else {
-        document.body.innerText = "Created by: [Your Name]";
+        document.body.innerText = "Created By Subin Dev";
     }
 }
 
-// Run the main function on page load
 main();
